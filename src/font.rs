8a6c41f886a88a6edec9e63a6ed6b4db51f3f347
SHUTROPIA <ilyashvy@gmail.com>
2024-01-13 01:40:05 +0100

    add ansi256 color support (#7)

--- src/font.rs
+++ src/font.rs
@@ -1,6 +1,4 @@
-use alacritty_terminal::vte::ansi::NamedColor;
 use iced::advanced::text;
-use iced::Color;
 use iced::{Font, Size, Theme};
 use iced_graphics::renderer::Renderer;
 use iced_tiny_skia::{Backend, Settings};
@@ -67,35 +65,4 @@ fn font_measure(font_size: f32) -> Size<f32> {
         },
         iced::widget::text::Shaping::Advanced,
     )
-}
-
-pub fn get_color(c: alacritty_terminal::vte::ansi::Color) -> Color {
-    match c {
-        alacritty_terminal::vte::ansi::Color::Spec(rgb) => {
-            Color::from_rgb8(rgb.r, rgb.g, rgb.b)
-        },
-        alacritty_terminal::vte::ansi::Color::Named(c) => match c {
-            NamedColor::Foreground => Color::from_rgb8(235, 218, 177),
-            NamedColor::Background => Color::from_rgb8(40, 39, 39),
-            NamedColor::Green => Color::from_rgb8(152, 150, 26),
-            NamedColor::Red => Color::from_rgb8(203, 35, 29),
-            NamedColor::Yellow => Color::from_rgb8(214, 152, 33),
-            NamedColor::Blue => Color::from_rgb8(69, 132, 135),
-            NamedColor::Cyan => Color::from_rgb8(104, 156, 105),
-            NamedColor::Magenta => Color::from_rgb8(176, 97, 133),
-            NamedColor::White => Color::from_rgb8(168, 152, 131),
-            NamedColor::Black => Color::from_rgb8(40, 39, 39),
-            NamedColor::BrightBlack => Color::from_rgb8(146, 130, 115),
-            NamedColor::BrightRed => Color::from_rgb8(250, 72, 52),
-            NamedColor::BrightGreen => Color::from_rgb8(184, 186, 38),
-            NamedColor::BrightYellow => Color::from_rgb8(249, 188, 47),
-            NamedColor::BrightBlue => Color::from_rgb8(131, 164, 151),
-            NamedColor::BrightMagenta => Color::from_rgb8(210, 133, 154),
-            NamedColor::BrightCyan => Color::from_rgb8(142, 191, 123),
-            NamedColor::BrightWhite => Color::from_rgb8(235, 218, 177),
-            NamedColor::BrightForeground => Color::from_rgb8(235, 218, 177),
-            _ => Color::from_rgb8(40, 39, 39),
-        },
-        _ => Color::from_rgb8(40, 39, 39),
-    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod backend;
 mod font;
 mod term;
+mod theme;
 
 pub use backend::BackendSettings;
 pub use font::FontSettings;
